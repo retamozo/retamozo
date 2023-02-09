cca8fd465b41cef63005c1360b4a7b5e20d43b55
retamozo <eretamozo97@gmail.com>
2023-02-09 11:23:38 -0300

    added timeline component (structure)

---
+++ src/components/Timeline/Timeline.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent, PropsWithChildren } from 'react';
+import { ProgressBar } from '../ProgressBar';
+
+
+
+const Timeline: FunctionComponent<PropsWithChildren> = () => {
+    return (
+        <div  >
+            <ProgressBar />
+        </div>
+    );
+};
+
+export default Timeline;
