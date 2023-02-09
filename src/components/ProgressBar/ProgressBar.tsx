7caf78c98c31b668363df455b98a5ebac16d7e9c
retamozo <eretamozo97@gmail.com>
2023-02-09 11:23:16 -0300

    added simple progress bar

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProgressBar = () => {
+    return (
+        <div className='flex h-96 justify-center'>
+            <div className='bg-slate-200 rounded-xl h-[100%]'>
+                <div className="bg-slate-700 w-full rounded-md">a</div>
+            </div>
+        </div>
+    );
+};
+
+export default ProgressBar;
